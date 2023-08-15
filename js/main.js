f4a99977e8676babcad7bd0c12e2168fc7f7d95b
Vlad <vd1153132@gmail.com>
2023-08-15 19:32:20 +0200

    first change timer

--- js/main.js
+++ js/main.js
@@ -1,36 +1,89 @@
 window.addEventListener('DOMContentLoaded', function() {
-    'use strict';
-    let tab = document.querySelectorAll('.info-header-tab'),
-    info = document.querySelector('.info-header'),
-    tabContent = document.querySelectorAll('.info-tabcontent');
+    // 'use strict';
+    // let tab = document.querySelectorAll('.info-header-tab'),
+    // info = document.querySelector('.info-header'),
+    // tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-        }
+    // function hideTabContent(a) {
+    //     for (let i = a; i < tabContent.length; i++) {
+    //         tabContent[i].classList.remove('show');
+    //         tabContent[i].classList.add('hide');
+    //     }
+    // };
+    // hideTabContent(1);
+
+    // function showTabContent(b) {
+    // if ( tabContent[b].classList.contains('hide')) {
+    //     tabContent[b].classList.remove('hide'); 
+    //     tabContent[b].classList.add('show'); 
+    //     }
+    // }
+
+    // info.addEventListener('click', function(event) {
+    //     let target = event.target;
+    //     if(target && target.classList.contains('info-header-tab')) {
+    //         for(let i = 0; i < tab.length; i++) {
+    //             if(target == tab[i]) {
+    //                 hideTabContent(0);
+    //                 showTabContent(i);
+    //                 console.log(i);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // });
+    //Timer
+    let deadLine = '2024-08-01';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/1000/60/60) % 24),
+        days = Math.floor(t/(1000*60*60*24));
+        
+        return {
+            'total': t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds,
+            'days' : days
+        };
     };
-    hideTabContent(1);
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        days = timer.querySelector('.days');
+        timeInterval = setInterval(updateClock, 1000);
 
-    function showTabContent(b) {
-    if ( tabContent[b].classList.contains('hide')) {
-        tabContent[b].classList.remove('hide'); 
-        tabContent[b].classList.add('show'); 
-        }
-    }
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
 
-    info.addEventListener('click', function(event) {
-        let target = event.target;
-        if(target && target.classList.contains('info-header-tab')) {
-            for(let i = 0; i < tab.length; i++) {
-                if(target == tab[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
-                    console.log(i);
-                    break;
-                }
+            function addZero(num){
+                if(num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+            days.textContent = addZero(t.days) + ' ' + 'd';
+            minutes.textContent = addZero(t.minutes) + ' ' + 'min';
+            seconds.textContent = addZero(t.seconds) + ' ' + 'sec';
+            hours.textContent = addZero(t.hours) + ' ' + 'h';
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            };
+            if(t.total <= 0) {
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             }
         }
-    });
 
+    };
+    setClock('timer', deadLine);
 });
+
